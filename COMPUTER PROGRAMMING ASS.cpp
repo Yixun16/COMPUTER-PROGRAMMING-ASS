ff1f253390857a1b3fc5893bf6ab2c9948a2214c
yixun User <yixun126@gmail.com>
2026-06-14 01:56:02 +0800

    Refine the comment significant line

--- COMPUTER PROGRAMMING ASS.cpp
+++ COMPUTER PROGRAMMING ASS.cpp
@@ -10,7 +10,7 @@ const int CONGESTION_LIMIT = 300;
 //structure declaration
 struct Traffic
 {
-	int vehiclesCounts;
+	int vehiclesCounts;	
 };
 
 //function header
@@ -22,10 +22,10 @@ void congestionPeriod(Traffic[]);
 
 int main()
 {
-	//declaration
+	//array declaration using CONST SIZE
 	Traffic roadA[SIZE];
 	Traffic roadB[SIZE];
-	string fileName = "Traffic_Data.txt";
+	string fileName = "Traffic_Data.txt"; // Define the fileName
 		
 	//Comfirmation of creating file
 	ofstream outFile;
@@ -37,7 +37,6 @@ int main()
 		exit(1);
 	}
 	
-
 	//Insert data Road A and Road B into text file
 	cout << "===== ENTER TRAFFIC DATA =====" << endl;
 	cout << "The data will be save into " << fileName << endl << endl << endl;
@@ -45,17 +44,17 @@ int main()
 
 	//Data Road A
 	cout << "===== ROAD A =====" << endl;
-		for (int i = 0; i < SIZE; i++)
+		for (int i = 0; i < SIZE; i++) //using loop to user input 5 period of data
 		{
 			int tempDataMemory = 0;
 			cout << "The vehicle count at period " << i + 1 << " is : ";
 			cin >> tempDataMemory;
-			outFile << tempDataMemory << endl;
+			outFile << tempDataMemory << endl; //data will be save into text file
 		}
 
 
 	//Data Road B
-	cout << endl << endl << "===== ROAD B =====" << endl;
+	cout << endl << endl << "===== ROAD B =====" << endl; //doing same thing as Road A
 		for (int i = 0; i < SIZE; i++)
 		{
 			int tempDataMemory = 0;
@@ -75,14 +74,14 @@ int main()
 	//Comfirmation of reading file
 	if (!inFile)
 	{
-		cout << "Unable to read file. " << endl;
+		cout << "Unable to read file. " << endl; //check if file to open properly
 		exit(1);
 	}
 
 	//Data A to array roadA
 	for (int i = 0; i < SIZE; i++)
 	{
-		inFile >> roadA[i].vehiclesCounts;
+		inFile >> roadA[i].vehiclesCounts; //taking data from text file into array
 	}
 
 	//Data B to array roadB
@@ -95,48 +94,48 @@ int main()
 
 	//Process of ROAD A
 	int totalRoadA = 0;
-	float aveRoadA = 0;
+	float aveRoadA = 0; //initialized for upcoming processes
 	int peakRoadA = 0;
 
 	totalRoadA = totalVehicles(roadA);
-	aveRoadA = averageVehicles(roadA);
+	aveRoadA = averageVehicles(roadA); //data returned by the function
 	peakRoadA = peakTrafficPeriod(roadA);
 
 	//Process of ROAD B
 	int totalRoadB = 0;
-	float aveRoadB = 0;
+	float aveRoadB = 0; // initialized for upcoming processes
 	int peakRoadB = 0;
 
 	totalRoadB = totalVehicles(roadB);
-	aveRoadB = averageVehicles(roadB);
+	aveRoadB = averageVehicles(roadB); //data returned by the function
 	peakRoadB = peakTrafficPeriod(roadB);
 
 
 	//Output report Road A
 	cout << endl << endl << endl << "===== ROAD A REPORT =====" << endl;
 	cout << "Total Vehicles : " << totalRoadA << endl;
-	cout << "Average Vehicles : " << fixed << setprecision(2) << aveRoadA << endl;
+	cout << "Average Vehicles : " << fixed << setprecision(2) << aveRoadA << endl; //using setprecision to control decimal places up to 2
 	cout << "Peak Traffic Period : " << peakRoadA << endl;
 	cout << endl << "Traffic Classification : " << endl;
-	classifyTrafficFlow(roadA);
+	classifyTrafficFlow(roadA); //calling out data from the function
 	cout << endl << "Traffic Congenstion Period : " << endl; 
-	congestionPeriod(roadA);
+	congestionPeriod(roadA); //calling out data from the function
 
 
 	//Output report Road B
 	cout << endl << endl << "===== ROAD B REPORT =====" << endl;
 	cout << "Total Vehicles : " << totalRoadB << endl;
-	cout << "Average Vehicles : " << fixed << setprecision(2) << aveRoadB << endl;
+	cout << "Average Vehicles : " << fixed << setprecision(2) << aveRoadB << endl; //using setprecision to control decimal places up to 2
 	cout << "Peak Traffic Period : " << peakRoadB << endl;
 	cout << endl << "Traffic Classification : " << endl;
-	classifyTrafficFlow(roadB) ;
+	classifyTrafficFlow(roadB) ; //calling out data from the function
 	cout << endl << "Traffic Congenstion Period : " << endl;
-	congestionPeriod(roadB);
+	congestionPeriod(roadB); //calling out data from the function
 
 
 	//Comparision of Road A and Road 
 	cout << endl << endl << "===== COMPARISION TRAFFIC FLOW BOTH ROAD =====" << endl;
-	if (totalRoadA > totalRoadB)
+	if (totalRoadA > totalRoadB) //using else if to compare both road
 	{
 		cout << "Road A having heavily traffic flow than road B " << endl << endl;
 	}
@@ -156,7 +155,7 @@ int main()
 // Function classify traffic flow
 void classifyTrafficFlow(Traffic road[])
 { 
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++) //using for loop to determine each period of traffic flow
 	{
 		cout << "period " << i + 1 << " : ";
 		if (road[i].vehiclesCounts < 100)
@@ -173,7 +172,7 @@ void classifyTrafficFlow(Traffic road[])
 int totalVehicles(Traffic road[])
 {
 	int total = 0;
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++) //using loop to sum up all period of vehicles counts
 	{
 		total += road[i].vehiclesCounts;
 	}
@@ -184,12 +183,12 @@ int totalVehicles(Traffic road[])
 float averageVehicles(Traffic road[])
 {
 	float total = 0;
-	float average = 0;
-	for (int i = 0; i < SIZE; i++)
+	float average = 0; 
+	for (int i = 0; i < SIZE; i++) //using loop to sum up all period of vehicles counts 
 	{
 		total += road[i].vehiclesCounts;
 	}
-	average = total / SIZE;
+	average = total / SIZE; // finding the average of vehicles
 	return average;
 }
 
@@ -197,10 +196,10 @@ float averageVehicles(Traffic road[])
 int peakTrafficPeriod(Traffic road[])
 {
 	int peak = 0;
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < SIZE; i++) // using loop compare all the period to find out the most vehicles counts period
 	{
 		if (road[i].vehiclesCounts > road[peak].vehiclesCounts)
-			peak = i;
+			peak = i; //update the peak if now value is larger than previous
 	}
 	return peak + 1;
 }
@@ -208,17 +207,17 @@ int peakTrafficPeriod(Traffic road[])
 // Function congestion
 void congestionPeriod(Traffic road[])
 {
-	bool congestionFound = false;
-	for (int i = 0; i < SIZE; i++)
+	bool congestionFound = false; // initial boolean flag as false
+	for (int i = 0; i < SIZE; i++) // looping the period inside array
 	{
-		if (road[i].vehiclesCounts >= CONGESTION_LIMIT)
+		if (road[i].vehiclesCounts >= CONGESTION_LIMIT) //compare each period vehicles counts
 		{
 			cout << "Congestion detected at period " << i + 1 << endl;
-			congestionFound = true;
+			congestionFound = true; //update the congestionFound become true
 		}
 	}
-	if (!congestionFound)
-		cout << "Congestion not detected " << endl;
+	if (!congestionFound) //condition of screen out congestion not detected
+		cout << "Congestion not detected " << endl; 
 
 	return;
 }
